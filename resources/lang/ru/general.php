b4bbefd564fd08d5bf09a69cbb7cf14240a8520b
Eugene <viiper94@gmail.com>
2020-08-21 13:21:46 +0300

    Russian language enabled, English removed

---
+++ resources/lang/ru/general.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+    'vtc_evoque' => 'ВТК EVOQUE'
+
+];
--- resources/lang/ru/passwords.php
+++ resources/lang/ru/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Ваш пароль был сброшен!',
+    'sent' => 'Ссылка для сброса пароля была отправлена на указанную почту!',
+    'throttled' => 'Пожалуйста, подождите перед повторной попыткой.',
+    'token' => 'Этот токен уже недействителен.',
+    'user' => "Нет пользователя с такой почтой.",
 
 ];

bc30dc6f8556b6f855dc3be1b1378d80a6d10c53
Eugene <viiper94@gmail.com>
2020-09-05 20:43:15 +0300

    updated users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,11 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('steamid64')->unique();
+            $table->string('truckersmp_id')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin');
             $table->rememberToken();
             $table->timestamps();
         });

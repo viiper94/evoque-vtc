dfc967b517f5d64ae8dff061211cd91bd1523d3b
Eugene <viiper94@gmail.com>
2020-09-19 20:38:37 +0300

    pluralized string

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -84,7 +84,7 @@
         <div class="feature col-md-4 col-sm-12">
             <h1 class="display-2 font-weight-bold">{{ $members_count }}</h1>
             <hr class="m-auto pb-3">
-            <p>опытных водителя</p>
+            <p>{{ \Illuminate\Support\Facades\Lang::choice('опытный водитель|опытных водителя|опытных водителей', $members_count) }}</p>
         </div>
     </section>
 

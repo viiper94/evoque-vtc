16b93c33fabbfb25be5fc8d0754bd8a5c8c2537c
Eugene <viiper94@gmail.com>
2020-09-19 23:31:08 +0300

    added no convoy note

--- resources/views/convoys.blade.php
+++ resources/views/convoys.blade.php
@@ -3,184 +3,189 @@
 @section('content')
 
 <div class="container pt-5">
-    <h1 class="convoy-title text-center pt-5 pb-5">{{ $convoy->title }} {{ $convoy->start_time->isoFormat('L') }}</h1>
-    <div class="row mb-5">
-        <section class="convoy-info col-md-6 col-sm-12 text-right">
-            <ul>
-                <li class="row justify-content-end">
-                    <div class="mr-4 mb-4">
-                        <p>Дата:</p>
-                        <h4>{{ $convoy->start_time->isoFormat('LL') }}</h4>
-                    </div>
-                    <div class="convoy-icon pl-5 pr-4 text-left">
-                        <i class="fas fa-calendar-alt"></i>
-                    </div>
-                </li>
-                <li class="row justify-content-end">
-                    <div class="mr-4 mb-4">
-                        <p>Место старта:</p>
-                        <h2>{{ $convoy->start }}</h2>
-                    </div>
-                    <div class="convoy-icon pl-5 pr-4 text-left">
-                        <i class="fas fa-map-marker"></i>
-                    </div>
-                </li>
-                <li class="row justify-content-end">
-                    <div class="mr-4 mb-4">
-                        <p>Место отдыха:</p>
-                        <h2>{{ $convoy->rest }}</h2>
-                    </div>
-                    <div class="convoy-icon pl-5 pr-4 text-left">
-                        <i class="fas fa-map-marked-alt"></i>
-                    </div>
-                </li>
-                <li class="row justify-content-end">
-                    <div class="mr-4 mb-4">
-                        <p>Место финиша:</p>
-                        <h2>{{ $convoy->finish }}</h2>
-                    </div>
-                    <div class="convoy-icon pl-5 pr-4 text-left">
-                        <i class="fas fa-map-marker-alt"></i>
-                    </div>
-                </li>
-                <li class="row justify-content-end">
-                    <div class="mr-4 mb-4">
-                        <p>Встречаемся на сервере:</p>
-                        <h4>{{ $convoy->server }}</h4>
-                    </div>
-                    <div class="convoy-icon pl-5 pr-4 text-left">
-                        <i class="fas fa-server"></i>
-                    </div>
-                </li>
-                <li class="row justify-content-end">
-                    <div class="mr-4 mb-4">
-                        <p>Начало конвоя:</p>
-                        <h4>{{ $convoy->start_time->subMinutes(30)->format('H:i') }} по МСК</h4>
-                    </div>
-                    <div class="convoy-icon pl-5 pr-4 text-left">
-                        <i class="fas fa-hourglass-half"></i>
-                    </div>
-                </li>
-                <li class="row justify-content-end">
-                    <div class="mr-4 mb-4">
-                        <p>Выезд с места старта:</p>
-                        <h4>{{ $convoy->start_time->format('H:i') }} по МСК</h4>
-                    </div>
-                    <div class="convoy-icon pl-5 pr-4 text-left">
-                        <i class="fas fa-clock"></i>
-                    </div>
-                </li>
-            </ul>
-        </section>
-        <section class="route col-md-6 col-sm-12">
-            <a href="{{ $convoy->route }}" target="_blank"><img src="{{ $convoy->route }}" class="text-shadow-m"></a>
-        </section>
-    </div>
-
-    <section class="convoy-note pb-5 pt-5 m-auto">
-        <hr class="m-auto">
-        <blockquote class="blockquote text-center mb-4 mt-4">
-            <p class="mb-0">Выставив маршрут, ваша поездка будет <br> комфортной и спокойной!</p>
-        </blockquote>
-        <hr class="m-auto">
-    </section>
-
-    <section class="row convoy-info pt-5 pb-5">
-        <ul class="col-md-6 col-sm-12 text-right mb-0">
-            <li class="row justify-content-end">
-                <div class="mr-4 mb-4">
-                    <p>Связь ведём через TeamSpeak 3:</p>
-                    <a href="http://invite.teamspeak.com/evoque.ts3srv.ru" target="_blank"><h2>evoque.ts3srv.ru</h2></a>
-                </div>
-                <div class="convoy-icon pl-5 pr-4 text-left">
-                    <i class="fab fa-teamspeak"></i>
-                </div>
-            </li>
-            <li class="row justify-content-end">
-                <div class="mr-4">
-                    <p>Канал на сервере:</p>
-                    <h4>Открытый конвой ВТК "EVOQUE"</h4>
-                </div>
-                <div class="convoy-icon pl-5 pr-4 text-left">
-                    <i class="fas fa-list-alt"></i>
-                </div>
-            </li>
-        </ul>
-        <div class="ts-download col-md-6 col-sm-12 row flex-column align-items-center justify-content-center">
-            <a href="https://teamspeak.com/ru/downloads/" target="_blank" class="btn btn-warning btn-lg mb-2">Скачать TeamSpeak 3</a>
-            <button data-toggle="modal" data-target="#ts3-overlay-modal" class="btn btn-outline-warning mt-2">Как настроить оверлей для TS3?</button>
-        </div>
-    </section>
-
-    <section class="convoy-note pb-5 pt-5 m-auto">
-        <hr class="m-auto">
-        <blockquote class="blockquote text-center mb-4 mt-4">
-            <p class="mb-0">В колонне держим дистанцию не менее 70 метров по TAB и соблюдаем правила TruckersMP.<br>Помимо рации, флуд, мат, а так же фоновая музыка запрещены и в канале TeamSpeak.</p>
-        </blockquote>
-        <hr class="m-auto">
-    </section>
-
-    @if(isset($convoy->trailer))
-        <div class="row mb-5 mt-5">
+    @if($convoy)
+        <h1 class="convoy-title text-center pt-5 pb-5">{{ $convoy->title }} {{ $convoy->start_time->isoFormat('L') }}</h1>
+        <div class="row mb-5">
             <section class="convoy-info col-md-6 col-sm-12 text-right">
-                <ul class="mb-0">
+                <ul>
                     <li class="row justify-content-end">
                         <div class="mr-4 mb-4">
-                            <p>Полуприцеп:</p>
-                            <h2>{{ $convoy->trailer }}</h2>
+                            <p>Дата:</p>
+                            <h4>{{ $convoy->start_time->isoFormat('LL') }}</h4>
                         </div>
                         <div class="convoy-icon pl-5 pr-4 text-left">
-                            <i class="fas fa-trailer"></i>
+                            <i class="fas fa-calendar-alt"></i>
                         </div>
                     </li>
-                    @if(isset($convoy->trailer_tuning))
-                        <li class="row justify-content-end">
-                            <div class="mr-4 mb-4">
-                                <p>Окрас:</p>
-                                <h4>{{ $convoy->trailer_tuning }}</h4>
-                            </div>
-                            <div class="convoy-icon pl-5 pr-4 text-left">
-                                <i class="fas fa-star"></i>
-                            </div>
-                        </li>
-                    @endif
-                    @if(isset($convoy->trailer_paint))
-                        <li class="row justify-content-end">
-                            <div class="mr-4 mb-4">
-                                <p>Окрас:</p>
-                                <h4>{{ $convoy->trailer_paint }}</h4>
-                            </div>
-                            <div class="convoy-icon pl-5 pr-4 text-left">
-                                <i class="fas fa-fill"></i>
-                            </div>
-                        </li>
-                    @endif
-                    @if(isset($convoy->trailer_cargo))
-                        <li class="row justify-content-end">
-                            <div class="mr-4 mb-4">
-                                <p>Груз:</p>
-                                <h4>{{ $convoy->trailer_cargo }}</h4>
-                            </div>
-                            <div class="convoy-icon pl-5 pr-4 text-left">
-                                <i class="fas fa-truck-loading"></i>
-                            </div>
-                        </li>
-                    @endif
+                    <li class="row justify-content-end">
+                        <div class="mr-4 mb-4">
+                            <p>Место старта:</p>
+                            <h2>{{ $convoy->start }}</h2>
+                        </div>
+                        <div class="convoy-icon pl-5 pr-4 text-left">
+                            <i class="fas fa-map-marker"></i>
+                        </div>
+                    </li>
+                    <li class="row justify-content-end">
+                        <div class="mr-4 mb-4">
+                            <p>Место отдыха:</p>
+                            <h2>{{ $convoy->rest }}</h2>
+                        </div>
+                        <div class="convoy-icon pl-5 pr-4 text-left">
+                            <i class="fas fa-map-marked-alt"></i>
+                        </div>
+                    </li>
+                    <li class="row justify-content-end">
+                        <div class="mr-4 mb-4">
+                            <p>Место финиша:</p>
+                            <h2>{{ $convoy->finish }}</h2>
+                        </div>
+                        <div class="convoy-icon pl-5 pr-4 text-left">
+                            <i class="fas fa-map-marker-alt"></i>
+                        </div>
+                    </li>
+                    <li class="row justify-content-end">
+                        <div class="mr-4 mb-4">
+                            <p>Встречаемся на сервере:</p>
+                            <h4>{{ $convoy->server }}</h4>
+                        </div>
+                        <div class="convoy-icon pl-5 pr-4 text-left">
+                            <i class="fas fa-server"></i>
+                        </div>
+                    </li>
+                    <li class="row justify-content-end">
+                        <div class="mr-4 mb-4">
+                            <p>Начало конвоя:</p>
+                            <h4>{{ $convoy->start_time->subMinutes(30)->format('H:i') }} по МСК</h4>
+                        </div>
+                        <div class="convoy-icon pl-5 pr-4 text-left">
+                            <i class="fas fa-hourglass-half"></i>
+                        </div>
+                    </li>
+                    <li class="row justify-content-end">
+                        <div class="mr-4 mb-4">
+                            <p>Выезд с места старта:</p>
+                            <h4>{{ $convoy->start_time->format('H:i') }} по МСК</h4>
+                        </div>
+                        <div class="convoy-icon pl-5 pr-4 text-left">
+                            <i class="fas fa-clock"></i>
+                        </div>
+                    </li>
                 </ul>
             </section>
             <section class="route col-md-6 col-sm-12">
-                <a href="{{ $convoy->trailer_image }}" target="_blank"><img src="{{ $convoy->trailer_image }}" class="text-shadow-m"></a>
+                <a href="{{ $convoy->route }}" target="_blank"><img src="{{ $convoy->route }}" class="text-shadow-m"></a>
             </section>
         </div>
+
+        <section class="convoy-note pb-5 pt-5 m-auto">
+            <hr class="m-auto">
+            <blockquote class="blockquote text-center mb-4 mt-4">
+                <p class="mb-0">Выставив маршрут, ваша поездка будет <br> комфортной и спокойной!</p>
+            </blockquote>
+            <hr class="m-auto">
+        </section>
+
+        <section class="row convoy-info pt-5 pb-5">
+            <ul class="col-md-6 col-sm-12 text-right mb-0">
+                <li class="row justify-content-end">
+                    <div class="mr-4 mb-4">
+                        <p>Связь ведём через TeamSpeak 3:</p>
+                        <a href="http://invite.teamspeak.com/evoque.ts3srv.ru" target="_blank"><h2>evoque.ts3srv.ru</h2></a>
+                    </div>
+                    <div class="convoy-icon pl-5 pr-4 text-left">
+                        <i class="fab fa-teamspeak"></i>
+                    </div>
+                </li>
+                <li class="row justify-content-end">
+                    <div class="mr-4">
+                        <p>Канал на сервере:</p>
+                        <h4>Открытый конвой ВТК "EVOQUE"</h4>
+                    </div>
+                    <div class="convoy-icon pl-5 pr-4 text-left">
+                        <i class="fas fa-list-alt"></i>
+                    </div>
+                </li>
+            </ul>
+            <div class="ts-download col-md-6 col-sm-12 row flex-column align-items-center justify-content-center">
+                <a href="https://teamspeak.com/ru/downloads/" target="_blank" class="btn btn-warning btn-lg mb-2">Скачать TeamSpeak 3</a>
+                <button data-toggle="modal" data-target="#ts3-overlay-modal" class="btn btn-outline-warning mt-2">Как настроить оверлей для TS3?</button>
+            </div>
+        </section>
+
+        <section class="convoy-note pb-5 pt-5 m-auto">
+            <hr class="m-auto">
+            <blockquote class="blockquote text-center mb-4 mt-4">
+                <p class="mb-0">В колонне держим дистанцию не менее 70 метров по TAB и соблюдаем правила TruckersMP.<br>Помимо рации, флуд, мат, а так же фоновая музыка запрещены и в канале TeamSpeak.</p>
+            </blockquote>
+            <hr class="m-auto">
+        </section>
+
+        @if(isset($convoy->trailer))
+            <div class="row mb-5 mt-5">
+                <section class="convoy-info col-md-6 col-sm-12 text-right">
+                    <ul class="mb-0">
+                        <li class="row justify-content-end">
+                            <div class="mr-4 mb-4">
+                                <p>Полуприцеп:</p>
+                                <h2>{{ $convoy->trailer }}</h2>
+                            </div>
+                            <div class="convoy-icon pl-5 pr-4 text-left">
+                                <i class="fas fa-trailer"></i>
+                            </div>
+                        </li>
+                        @if(isset($convoy->trailer_tuning))
+                            <li class="row justify-content-end">
+                                <div class="mr-4 mb-4">
+                                    <p>Окрас:</p>
+                                    <h4>{{ $convoy->trailer_tuning }}</h4>
+                                </div>
+                                <div class="convoy-icon pl-5 pr-4 text-left">
+                                    <i class="fas fa-star"></i>
+                                </div>
+                            </li>
+                        @endif
+                        @if(isset($convoy->trailer_paint))
+                            <li class="row justify-content-end">
+                                <div class="mr-4 mb-4">
+                                    <p>Окрас:</p>
+                                    <h4>{{ $convoy->trailer_paint }}</h4>
+                                </div>
+                                <div class="convoy-icon pl-5 pr-4 text-left">
+                                    <i class="fas fa-fill"></i>
+                                </div>
+                            </li>
+                        @endif
+                        @if(isset($convoy->trailer_cargo))
+                            <li class="row justify-content-end">
+                                <div class="mr-4 mb-4">
+                                    <p>Груз:</p>
+                                    <h4>{{ $convoy->trailer_cargo }}</h4>
+                                </div>
+                                <div class="convoy-icon pl-5 pr-4 text-left">
+                                    <i class="fas fa-truck-loading"></i>
+                                </div>
+                            </li>
+                        @endif
+                    </ul>
+                </section>
+                <section class="route col-md-6 col-sm-12">
+                    <a href="{{ $convoy->trailer_image }}" target="_blank"><img src="{{ $convoy->trailer_image }}" class="text-shadow-m"></a>
+                </section>
+            </div>
+        @endif
+
+        <section class="convoy-note pb-5 m-auto">
+            <hr class="m-auto">
+            <blockquote class="blockquote text-center mb-4 mt-4">
+                <p class="mb-0">Один груз — одна большая команда!</p>
+            </blockquote>
+            <hr class="m-auto">
+        </section>
+    @else
+        <h1 class="text-center text-primary p-4">Близжайших открытых конвоев еще нет!</h1>
     @endif
 
-    <section class="convoy-note pb-5 m-auto">
-        <hr class="m-auto">
-        <blockquote class="blockquote text-center mb-4 mt-4">
-            <p class="mb-0">Один груз — одна большая команда!</p>
-        </blockquote>
-        <hr class="m-auto">
-    </section>
 
 </div>
 

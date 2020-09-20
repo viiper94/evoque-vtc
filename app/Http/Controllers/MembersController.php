996c178007be4778022c35592cb1403c29ad7dc8
Eugene <viiper94@gmail.com>
2020-09-20 21:54:08 +0300

    added validation

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Member;
 use App\Role;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
@@ -20,9 +21,20 @@ class MembersController extends Controller{
     public function edit(Request $request, $id){
         if(Gate::denies('manage_members')) abort(403);
         if($request->post()){
+            $this->validate($request, [
+                'nickname' => 'required|string',
+                'join_date' => 'required|date_format:d.m.Y',
+                'convoys' => 'required|numeric',
+                'scores' => 'required|numeric',
+                'money' => 'required|numeric',
+                'vacations' => 'required|numeric',
+                'plate' => 'required|url',
+            ]);
             $member = Member::findOrFail($id);
             $member->fill($request->post());
             $member->visible = $request->input('visible') === 'on';
+            $member->join_date = Carbon::parse($request->input('join_date'))->format('Y-m-d');
+            $member->on_vacation_till = $request->input('on_vacation_till') ? Carbon::parse($request->input('on_vacation_till'))->format('Y-m-d') : null;
             $member->role()->detach();
             foreach($request->input('roles') as $role){
                 $member->role()->attach($role);

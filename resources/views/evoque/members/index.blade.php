ba9c82452647e456cccf4a5db6ca0c308b11e1de
Eugene <viiper94@gmail.com>
2020-09-20 17:38:13 +0300

    fix displaying members

--- resources/views/evoque/members/index.blade.php
+++ resources/views/evoque/members/index.blade.php
@@ -14,7 +14,7 @@
                 <th scope="col">Должность</th>
                 <th scope="col">Баллы</th>
                 <th scope="col">Эвики</th>
-                <th scope="col">Конвоев<br>за<br>неделю</th>
+                <th scope="col">Конвоев<br>за неделю</th>
                 <th scope="col">В отпуске до</th>
                 <th scope="col">Использовано<br>отпусков</th>
                 <th scope="col">Номер</th>
@@ -31,42 +31,44 @@
                 <tr>
                     <th colspan="14" class="text-center text-primary">{{ $role_group[0]->group }}</th>
                 </tr>
-                @foreach($role_group[0]->members as $member)
-                    @if($member->topRole() == $role_group[0]->id)
-                        <tr>
-                            <td>{{ $i++ }}</td>
-                            <td>{{ $member->nickname }}</td>
-                            <td>{{ $member->user->birth_date ? $member->user->birth_date->age : '–' }}</td>
-                            <td>
-                                @foreach($member->role as $item)
-                                    {{ $item->title }}@if(!$loop->last),@endif
-                                @endforeach
-                            </td>
-                            <td> @cannot('admin'){{ $member->scores }} @else {{ '∞' }} @endcannot </td>
-                            <td>{{ $member->isOwner() ? '∞' : $member->money }}</td>
-                            <td>{{ $member->convoys }}</td>
-                            <td>{{ !isset($member->on_vacation_till) ? '–' : $member->on_vacation_till->isoFormat('LL') }}</td>
-                            <td>{{ $member->vacations }}</td>
-                            <td class="plate-img p-0"><img src="{{ $member->plate }}"></td>
-                            <td><a href="{{ route('profile') }}/{{ $member->user->id }}" target="_blank">{{ $member->user->name }}</a></td>
-                            <td>{{ $member->getPlace() }}</td>
-                            <td class="icon-link">
-                                @isset($member->user->vk_link)
-                                    <a href="{{ $member->user->vk_link }}" target="_blank" class="mr-3"><i class="fab fa-vk"></i></a>
-                                @endisset
-                                @isset($member->user->steam_link)
-                                    <a href="{{ $member->user->steam_link }}" target="_blank" class="mr-3"><i class="fab fa-steam-square"></i></a>
-                                @endisset
-                                @isset($member->user->tmp_link)
-                                    <a href="{{ $member->user->tmp_link }}" target="_blank"><i class="fas fa-truck-pickup"></i></a>
-                                @endisset
-                                @can('manage_members')
-                                    <a href="{{ route('evoque.admin.members.edit', $member->id) }}" class="ml-3"><i class="fas fa-user-edit"></i></a>
-                                @endcan
-                            </td>
-                            <td>{{ !isset($member->join_date) ? '–' : $member->join_date->isoFormat('LL') }}</td>
-                        </tr>
-                    @endif
+                @foreach($role_group as $role)
+                    @foreach($role->members as $member)
+                        @if($member->topRole() == $role->id)
+                            <tr>
+                                <td>{{ $i++ }}</td>
+                                <td>{{ $member->nickname }}</td>
+                                <td>{{ $member->user->birth_date ? $member->user->birth_date->age : '–' }}</td>
+                                <td>
+                                    @foreach($member->role as $item)
+                                        {{ $item->title }}@if(!$loop->last),@endif
+                                    @endforeach
+                                </td>
+                                <td> @cannot('admin'){{ $member->scores }} @else {{ '∞' }} @endcannot </td>
+                                <td>{{ $member->isOwner() ? '∞' : $member->money }}</td>
+                                <td>{{ $member->convoys }}</td>
+                                <td>{{ !isset($member->on_vacation_till) ? '–' : $member->on_vacation_till->isoFormat('LL') }}</td>
+                                <td>{{ $member->vacations }}</td>
+                                <td class="plate-img p-0"><img src="{{ $member->plate }}"></td>
+                                <td><a href="{{ route('profile') }}/{{ $member->user->id }}" target="_blank">{{ $member->user->name }}</a></td>
+                                <td>{{ $member->getPlace() }}</td>
+                                <td class="icon-link">
+                                    @isset($member->user->vk_link)
+                                        <a href="{{ $member->user->vk_link }}" target="_blank" class="mr-3"><i class="fab fa-vk"></i></a>
+                                    @endisset
+                                    @isset($member->user->steam_link)
+                                        <a href="{{ $member->user->steam_link }}" target="_blank" class="mr-3"><i class="fab fa-steam-square"></i></a>
+                                    @endisset
+                                    @isset($member->user->tmp_link)
+                                        <a href="{{ $member->user->tmp_link }}" target="_blank"><i class="fas fa-truck-pickup"></i></a>
+                                    @endisset
+                                    @can('manage_members')
+                                        <a href="{{ route('evoque.admin.members.edit', $member->id) }}" class="ml-3"><i class="fas fa-user-edit"></i></a>
+                                    @endcan
+                                </td>
+                                <td>{{ !isset($member->join_date) ? '–' : $member->join_date->isoFormat('LL') }}</td>
+                            </tr>
+                        @endif
+                    @endforeach
                 @endforeach
             @endforeach
             </tbody>

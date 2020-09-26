7b70e48411655ceab1bc78c4a9c7e916dab16cb1
Eugene <viiper94@gmail.com>
2020-09-26 18:22:09 +0300

    added vk link and fixed profile link

--- resources/views/evoque/users.blade.php
+++ resources/views/evoque/users.blade.php
@@ -5,16 +5,17 @@
     <div class="container-fluid pt-5">
         @include('layout.alert')
         <h2 class="pt-3 text-center text-primary">Все пользователи</h2>
-        <table class="users table table-dark table-hover"><thead>
+        <table class="users table table-dark table-hover text-center"><thead>
             <tr>
                 <th scope="col">Ава</th>
                 <th scope="col">Имя</th>
                 <th scope="col">Город</th>
                 <th scope="col">Страна</th>
                 <th scope="col">Дата рождения</th>
+                <th scope="col">VK</th>
                 <th scope="col">SteamID64</th>
                 <th scope="col">TruckersMP ID</th>
-                <th scope="col">Эвок</th>
+                <th scope="col">В компании</th>
                 <th scope="col">На сайте с</th>
                 <th scope="col">#</th>
             </tr>
@@ -23,10 +24,11 @@
             @foreach($users as $user)
                 <tr>
                     <td class="user-image"><img src="{{ $user->image }}"></td>
-                    <td><a href="{{ route('profile') }}/{{ $user->id }}">{{ $user->name }}</td>
+                    <td><a href="{{ route('evoque.profile', $user->id) }}">{{ $user->name }}</td>
                     <td>{{ $user->city }}</td>
                     <td>{{ $user->country }}</td>
                     <td>{{ !isset($user->birth_date) ? '' : $user->birth_date->isoFormat('LL') }}</td>
+                    <td><a href="{{ $user->vk }}" target="_blank">{{ $user->vk }}</a></td>
                     <td><a href="https://steamcommunity.com/profiles/{{ $user->steamid64 }}" target="_blank">{{ $user->steamid64 }}</a></td>
                     <td><a href="https://truckersmp.com/user/{{ $user->truckersmp_id }}" target="_blank">{{ $user->truckersmp_id }}</a></td>
                     <td><i class="fas fa-{{ $user->member ? 'check' : 'times' }}"></i></td>

8c4e19f798cb8b7dcab34bfd2116c5500ef32ee4
Eugene <viiper94@gmail.com>
2020-10-09 14:33:24 +0300

    add grid to member edit page

--- resources/views/evoque/members/edit.blade.php
+++ resources/views/evoque/members/edit.blade.php
@@ -15,41 +15,71 @@
         <h2 class="mt-3 text-primary">Редактирование сотрудника {{ $member->nickname }}</h2>
         <form method="post" class="mb-5 edit-member">
             @csrf
-            <div class="form-group">
-                <label for="nickname">Игровой ник</label>
-                <input type="text" class="form-control" id="nickname" name="nickname" value="{{ $member->nickname }}" required>
-                @if($errors->has('nickname'))
-                    <small class="form-text">{{ $errors->first('nickname') }}</small>
-                @endif
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="nickname">Игровой ник</label>
+                        <input type="text" class="form-control" id="nickname" name="nickname" value="{{ $member->nickname }}" required>
+                        @if($errors->has('nickname'))
+                            <small class="form-text">{{ $errors->first('nickname') }}</small>
+                        @endif
+                    </div>
+                    <div class="custom-control custom-checkbox mb-2">
+                        <input type="checkbox" class="custom-control-input" id="visible" name="visible" @if($member->visible) checked @endif>
+                        <label class="custom-control-label" for="visible">Виден на сайте</label>
+                    </div>
+                    <div class="form-group">
+                        <label for="join_date">Дата присоединения</label>
+                        <input type="text" class="form-control" id="join_date" name="join_date" value="{{ $member->join_date->format('d.m.Y') }}" autocomplete="off">
+                        @if($errors->has('join_date'))
+                            <small class="form-text">{{ $errors->first('join_date') }}</small>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label for="plate">Номерной знак (ссылка на изображение)</label>
+                        <input type="url" class="form-control" id="plate" name="plate" value="{{ $member->plate }}">
+                        @if($errors->has('plate'))
+                            <small class="form-text">{{ $errors->first('plate') }}</small>
+                        @endif
+                        @isset($member->plate) <img class="mt-1" src="{{ $member->plate }}"> @endisset
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="roles">Должность</label>
+                        <select multiple class="form-control" id="roles" name="roles[]" size="14">
+                            @foreach($roles as $role)
+                                <option value="{{ $role->id }}" @if($member->role->contains($role->id)) selected @endif >{{ $role->title }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="custom-control custom-checkbox mb-2">
+                        <input type="checkbox" class="custom-control-input" id="sort" name="sort" @if($member->sort) checked @endif>
+                        <label class="custom-control-label" for="sort">Показывать вверху списка</label>
+                    </div>
+                </div>
             </div>
-            <div class="custom-control custom-checkbox mb-2">
-                <input type="checkbox" class="custom-control-input" id="visible" name="visible" @if($member->visible) checked @endif>
-                <label class="custom-control-label" for="visible">Виден на сайте</label>
+            <div class="row">
+                <h5 class="col-12">Игровая статистика</h5>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="scores">Баллы</label>
+                        <input type="number" class="form-control" id="scores" name="scores" value="{{ $member->scores }}" placeholder="∞">
+                        @if($errors->has('scores'))
+                            <small class="form-text">{{ $errors->first('scores') }}</small>
+                        @endif
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="money">Эвики</label>
+                        <input type="text" class="form-control" id="money" name="money" value="{{ $member->money }}" placeholder="∞">
+                        @if($errors->has('money'))
+                            <small class="form-text">{{ $errors->first('money') }}</small>
+                        @endif
+                    </div>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="roles">Должность</label>
-                <select multiple class="form-control" id="roles" name="roles[]" size="14">
-                    @foreach($roles as $role)
-                        <option value="{{ $role->id }}" @if($member->role->contains($role->id)) selected @endif >{{ $role->title }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="join_date">Дата присоединения</label>
-                <input type="text" class="form-control" id="join_date" name="join_date" value="{{ $member->join_date->format('d.m.Y') }}" autocomplete="off">
-                @if($errors->has('join_date'))
-                    <small class="form-text">{{ $errors->first('join_date') }}</small>
-                @endif
-            </div>
-            <div class="form-group">
-                <label for="plate">Номерной знак (ссылка на изображение)</label>
-                <input type="url" class="form-control" id="plate" name="plate" value="{{ $member->plate }}">
-                @if($errors->has('plate'))
-                    <small class="form-text">{{ $errors->first('plate') }}</small>
-                @endif
-                @isset($member->plate) <img class="mt-1" src="{{ $member->plate }}"> @endisset
-            </div>
-            <h5>Игровая статистика</h5>
             <div class="form-group">
                 <label for="convoys">Посещение конвоев</label>
                 <input type="number" class="form-control" id="convoys" name="convoys" value="{{ $member->convoys }}" required>
@@ -57,38 +87,26 @@
                     <small class="form-text">{{ $errors->first('convoys') }}</small>
                 @endif
             </div>
-            <div class="custom-control custom-checkbox mb-2">
-                <input type="checkbox" class="custom-control-input" id="sort" name="sort" @if($member->sort) checked @endif>
-                <label class="custom-control-label" for="sort">Показывать вверху списка</label>
-            </div>
-            <div class="form-group">
-                <label for="scores">Баллы</label>
-                <input type="number" class="form-control" id="scores" name="scores" value="{{ $member->scores }}" placeholder="∞">
-                @if($errors->has('scores'))
-                    <small class="form-text">{{ $errors->first('scores') }}</small>
-                @endif
-            </div>
-            <div class="form-group">
-                <label for="money">Эвики</label>
-                <input type="text" class="form-control" id="money" name="money" value="{{ $member->money }}" placeholder="∞">
-                @if($errors->has('money'))
-                    <small class="form-text">{{ $errors->first('money') }}</small>
-                @endif
-            </div>
-            <h5>Отпуски</h5>
-            <div class="form-group">
-                <label for="vacations">Использовано отпусков</label>
-                <input type="number" class="form-control" id="vacations" name="vacations" value="{{ $member->vacations }}">
-                @if($errors->has('vacations'))
-                    <small class="form-text">{{ $errors->first('vacations') }}</small>
-                @endif
-            </div>
-            <div class="form-group">
-                <label for="on_vacation_till">В отпуске до</label>
-                <input type="text" class="form-control" id="on_vacation_till" name="on_vacation_till" value="{{ $member->on_vacation_till ? $member->on_vacation_till->format('d.m.Y') : '' }}" autocomplete="off">
-                @if($errors->has('on_vacation_till'))
-                    <small class="form-text">{{ $errors->first('on_vacation_till') }}</small>
-                @endif
+            <div class="row">
+                <h5 class="col-12">Отпуски</h5>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="vacations">Использовано отпусков</label>
+                        <input type="number" class="form-control" id="vacations" name="vacations" value="{{ $member->vacations }}">
+                        @if($errors->has('vacations'))
+                            <small class="form-text">{{ $errors->first('vacations') }}</small>
+                        @endif
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="on_vacation_till">В отпуске до</label>
+                        <input type="text" class="form-control" id="on_vacation_till" name="on_vacation_till" value="{{ $member->on_vacation_till ? $member->on_vacation_till->format('d.m.Y') : '' }}" autocomplete="off">
+                        @if($errors->has('on_vacation_till'))
+                            <small class="form-text">{{ $errors->first('on_vacation_till') }}</small>
+                        @endif
+                    </div>
+                </div>
             </div>
             <div class="row justify-content-center">
                 <button type="submit" class="btn btn-outline-warning btn-lg"><i class="fas fa-save"></i> Сохранить</button>

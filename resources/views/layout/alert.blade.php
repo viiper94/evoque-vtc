07a30f7455863785b6855ef3a1f5b1e94c0232f7
Eugene <viiper94@gmail.com>
2020-10-11 22:24:04 +0300

    removed padding

--- resources/views/layout/alert.blade.php
+++ resources/views/layout/alert.blade.php
@@ -1,10 +1,10 @@
 @if(session()->get('success'))
-    <div class="container pt-5">
+    <div class="container">
         <div class="alert alert-success" role="alert">{{ session()->get('success') }}</div>
     </div>
 @endif
 @if($errors->any())
-    <div class="container pt-5">
+    <div class="container">
         <div class="alert alert-danger" role="alert"><b>Ошибка:</b><br>{{ $errors->first() }}</div>
     </div>
 @endif

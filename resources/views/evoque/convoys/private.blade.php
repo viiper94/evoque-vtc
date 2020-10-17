6e565f657647997e56e414d0b50a282e27edf4e1
Eugene <viiper94@gmail.com>
2020-10-17 17:36:21 +0300

    reverted private convoy page layout

--- resources/views/evoque/convoys/private.blade.php
+++ resources/views/evoque/convoys/private.blade.php
@@ -65,102 +65,88 @@
                     @if($convoy->dlc)
                         <h4 class="pt-5 pb-5 text-center"><i class="fas fa-exclamation-triangle text-warning"></i> Для участия требуется DLC {{ implode(', ', $convoy->dlc) }}</h4>
                     @endif
-                    <div class="row justify-content-between convoy-info pt-5 pb-5 text-center">
-                        <div class="col text-center">
+                    <div class="row convoy-info pt-5">
+                        <div class="col-sm-6 pr-md-5 pr-3 text-md-right text-center">
                             <p>Тягач:</p>
                             <h3>{{ $convoy->truck ?? 'Любой' }}</h3>
-                        </div>
-                        @if($convoy->truck_tuning)
-                            <div class="col text-center">
+                            @if($convoy->truck_image)
+                                <a href="/images/convoys/{{ $convoy->truck_image }}" target="_blank"><img src="/images/convoys/{{ $convoy->truck_image }}" alt="{{ $convoy->truck }}" class="text-shadow-m"></a>
+                            @endif
+                            @if($convoy->truck_tuning)
                                 <p>Тюнинг:</p>
                                 <h3>{{ $convoy->truck_tuning }}</h3>
-                            </div>
-                        @endif
-                        @if($convoy->truck_paint)
-                            <div class="col text-center">
+                            @endif
+                            @if($convoy->truck_paint)
                                 <p>Окрас:</p>
                                 <h3>{{ $convoy->truck_paint }}</h3>
-                            </div>
-                        @endif
-                        @if($convoy->truck_image)
-                            <img src="/images/convoys/{{ $convoy->truck_image }}" alt="{{ $convoy->truck }}" class="text-shadow-m">
-                        @endif
-                    </div>
-                    <div class="row justify-content-between convoy-info pt-5">
-                        <div class="col text-center">
-                            <p>Прицеп:</p>
-                            <h3>{{ $convoy->trailer ?? 'Любой' }}</h3>
-                            @if($convoy->alt_trailer)
-                                <p>Прицеп без ДЛС:</p>
-                                <h4>{{ $convoy->alt_trailer }}</h4>
                             @endif
                         </div>
-                        @if($convoy->trailer_tuning)
-                            <div class="col text-center">
+                        <div class="col-sm-6 pl-md-5 pl-3 text-md-left text-center">
+                            <p>Прицеп:</p>
+                            <h3>{{ $convoy->trailer ?? 'Любой' }}</h3>
+                            @if($convoy->trailer_image)
+                                <a href="/images/convoys/{{ $convoy->trailer_image }}" target="_blank"><img src="/images/convoys/{{ $convoy->trailer_image }}" alt="{{ $convoy->trailer }}" class="text-shadow-m"></a>
+                            @endif
+                            @if($convoy->trailer_tuning)
                                 <p>Тюнинг:</p>
                                 <h3>{{ $convoy->trailer_tuning }}</h3>
-                                @if($convoy->alt_trailer_tuning)
-                                    <p>Тюнинг прицепа без ДЛС:</p>
-                                    <h4>{{ $convoy->alt_trailer_tuning }}</h4>
-                                @endif
-                            </div>
-                        @endif
-                        @if($convoy->trailer_paint)
-                            <div class="col text-center">
+                            @endif
+                            @if($convoy->trailer_paint)
                                 <p>Окрас:</p>
                                 <h3>{{ $convoy->trailer_paint }}</h3>
-                                @if($convoy->alt_trailer_paint)
-                                    <p>Окрас прицепа без ДЛС:</p>
-                                    <h4>{{ $convoy->alt_trailer_paint }}</h4>
-                                @endif
-                            </div>
-                        @endif
-                        @if($convoy->cargo)
-                            <div class="col text-center">
+                            @endif
+                            @if($convoy->cargo)
                                 <p>Груз:</p>
                                 <h3>{{ $convoy->cargo }}</h3>
-                                @if($convoy->alt_cargo)
-                                    <p>Груз без ДЛС:</p>
-                                    <h4>{{ $convoy->alt_cargo }}</h4>
-                                @endif
-                            </div>
-                        @endif
-                        @if($convoy->trailer_image)
-                            <div class="fotorama" data-allowfullscreen="true" data-nav="thumbs">
-                                <img src="/images/convoys/{{ $convoy->trailer_image }}" alt="{{ $convoy->trailer }}">
+                            @endif
+                            @if($convoy->alt_trailer)
+                                <p class="mt-5">Прицеп без ДЛС:</p>
+                                <h3>{{ $convoy->alt_trailer }}</h3>
                                 @if($convoy->alt_trailer_image)
-                                    <img src="/images/convoys/{{ $convoy->alt_trailer_image }}" alt="{{ $convoy->alt_trailer }}">
+                                    <a href="/images/convoys/{{ $convoy->alt_trailer_image }}" target="_blank"><img src="/images/convoys/{{ $convoy->alt_trailer_image }}" alt="{{ $convoy->alt_trailer }}" class="text-shadow-m"></a>
                                 @endif
-                            </div>
-                        @endif
-                    </div>
-                    <section class="convoy-note pb-5 pt-5 m-auto">
-                        <hr class="m-auto">
-                        <blockquote class="blockquote text-center mb-4 mt-4">
-                            <h5 class="mb-0">Правила ВТК на конвое:</h5>
-                            <ol class="text-left ml-5 mr-1">
-                                <li>Канал в рации <b>№7</b> (многочисленный флуд и музыка в рации полностью запрещены).</li>
-                                <li>Движение по команде и только колонной, соблюдая ПДД и правила мультиплеера.</li>
-                                <li>Рекомендуемая дистанция между машинами <b>70-150 метров</b>.</li>
-                                <li>Опережение и обгон c разрешения ведущего.</li>
-                                <li>Для того чтобы встать в колонну обязательно разрешение ведущего,<br>
-                                    после одобрения встаёте строго перед замыкающим.</li>
-                                <li>Обязателен <b>ближний свет фар</b> в любое время суток.</li>
-                                <li>Если перед вами кто-то залагал или остановился, то обходите его без остановки.</li>
-                                <li>После конвоя переходим в наш ТС для подведения итогов (пока ведущий не отпустит,<br>
-                                    из ТС не выходим ибо конвой засчитан не будет!</li>
-                            </ol>
-                        </blockquote>
-                        <hr class="m-auto">
-                    </section>
-                    <section class="w-100">
-                        <h1 class="text-center text-primary">Маршрут конвоя</h1>
-                        <div class="fotorama" data-allowfullscreen="true" data-nav="thumbs">
-                            @foreach($convoy->route as $item)
-                                <img src="/images/convoys/{{ $item }}">
-                            @endforeach
+                                @if($convoy->alt_trailer_tuning)
+                                    <p>Тюнинг:</p>
+                                    <h3>{{ $convoy->alt_trailer_tuning }}</h3>
+                                @endif
+                                @if($convoy->alt_trailer_paint)
+                                    <p>Окрас:</p>
+                                    <h3>{{ $convoy->alt_trailer_paint }}</h3>
+                                @endif
+                                @if($convoy->alt_cargo)
+                                    <p>Груз:</p>
+                                    <h3>{{ $convoy->alt_cargo }}</h3>
+                                @endif
+                            @endif
                         </div>
-                    </section>
+                        <section class="convoy-note pb-5 pt-5 m-auto">
+                            <hr class="m-auto">
+                            <blockquote class="blockquote text-center mb-4 mt-4">
+                                <h5 class="mb-0">Правила ВТК на конвое:</h5>
+                                <ol class="text-left ml-5 mr-1">
+                                    <li>Канал в рации <b>№7</b> (многочисленный флуд и музыка в рации полностью запрещены).</li>
+                                    <li>Движение по команде и только колонной, соблюдая ПДД и правила мультиплеера.</li>
+                                    <li>Рекомендуемая дистанция между машинами <b>70-150 метров</b>.</li>
+                                    <li>Опережение и обгон c разрешения ведущего.</li>
+                                    <li>Для того чтобы встать в колонну обязательно разрешение ведущего,<br>
+                                        после одобрения встаёте строго перед замыкающим.</li>
+                                    <li>Обязателен <b>ближний свет фар</b> в любое время суток.</li>
+                                    <li>Если перед вами кто-то залагал или остановился, то обходите его без остановки.</li>
+                                    <li>После конвоя переходим в наш ТС для подведения итогов (пока ведущий не отпустит,<br>
+                                        из ТС не выходим ибо конвой засчитан не будет!</li>
+                                </ol>
+                            </blockquote>
+                            <hr class="m-auto">
+                        </section>
+                        <section class="w-100">
+                            <h1 class="text-center text-primary">Маршрут конвоя</h1>
+                            <div class="fotorama" data-allowfullscreen="true" data-nav="thumbs">
+                                @foreach($convoy->route as $item)
+                                    <img src="/images/convoys/{{ $item }}">
+                                @endforeach
+                            </div>
+                        </section>
+                    </div>
                 </div>
             @endforeach
         @endforeach

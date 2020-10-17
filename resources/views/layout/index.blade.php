cc4ee3a0db31c321feac5caf001ed5a6006d075a
Eugene <viiper94@gmail.com>
2020-10-17 11:03:15 +0300

    added google analytics

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -5,6 +5,15 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139146641-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-139146641-2');
+    </script>
     <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
     <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}" media="screen,projection"/>
     @include('layout.scripts')

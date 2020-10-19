eb0c4b240282ddd15c6aac25313f12a2260995e6
Eugene <viiper94@gmail.com>
2020-10-19 17:48:08 +0300

    fix crash when logged user has no member assigned

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,50 +26,64 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Gate::define('admin', function($user){
-            foreach($user->member->role as $role){
-                if($role->admin) return true;
+            if($user->member){
+                foreach($user->member->role as $role){
+                    if($role->admin) return true;
+                }
             }
             return false;
         });
 
         Gate::define('manage_members', function($user){
-            foreach($user->member->role as $role){
-                if($role->manage_members || $role->admin) return true;
+            if($user->member){
+                foreach($user->member->role as $role){
+                    if($role->manage_members || $role->admin) return true;
+                }
             }
             return false;
         });
 
         Gate::define('manage_convoys', function($user){
-            foreach($user->member->role as $role){
-                if($role->manage_convoys || $role->admin) return true;
+            if($user->member){
+                foreach($user->member->role as $role){
+                    if($role->manage_convoys || $role->admin) return true;
+                }
             }
             return false;
         });
 
         Gate::define('lead_convoys', function($user){
-//            foreach($user->member->role as $role){
-//                if($role->lead_convoys || $role->admin) return true;
-//            }
+            if($user->member){
+                foreach($user->member->role as $role){
+                    if($role->lead_convoys || $role->admin) return true;
+                }
+            }
             return true;
         });
 
         Gate::define('manage_table', function($user){
-            foreach($user->member->role as $role){
-                if($role->manage_members || $role->manage_table || $role->admin) return true;
+            if($user->member){
+                foreach($user->member->role as $role){
+                    if($role->manage_members || $role->manage_table || $role->admin) return true;
+                }
             }
             return false;
         });
 
         Gate::define('manage_rp', function($user){
-            foreach($user->member->role as $role){
-                if($role->manage_rp || $role->admin) return true;
+            if($user->member){
+                foreach($user->member->role as $role){
+                    if($role->manage_rp || $role->admin) return true;
+                }
             }
             return false;
         });
 
         Gate::define('do_rp', function($user){
-            foreach($user->member->role as $role){
-                if($role->do_rp && !$user->member->onVacation() || $role->admin) return true;
+            if($user->member){
+                foreach($user->member->role as $role){
+                    if($role->do_rp && !$user->member->onVacation() || $role->admin) return true;
+                }
             }
             return false;
         });

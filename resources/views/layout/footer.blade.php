9399669c0ed29bc6ad823c819f0d810093ff161f
Eugene <viiper94@gmail.com>
2020-11-02 10:59:49 +0200

    changed order of blocks in footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -2,8 +2,21 @@
     <div class="container pt-5 pb-5 pl-5 pr-5">
         <div class="row">
             <div class="col-md-3 col-sm-12 row flex-column justify-content-center align-items-center ml-0">
-                <h2>@lang('general.vtc_evoque')</h2>
-                <span class="copyright">Все права защищены &copy; {{ date('Y') }}</span>
+                <h2>Наши контакты</h2>
+                <div class="contacts row">
+                    <div class="vk">
+                        <a href="https://vk.com/evoquevtc" target="_blank" class="pl-3 pr-2"><i class="fab fa-vk"></i></a>
+                    </div>
+                    <div class="discord">
+                        <a href="https://discord.com/invite/Gj53a8d" target="_blank" class="pl-3 pr-2"><i class="fab fa-discord"></i></a>
+                    </div>
+                    <div class="teamspeak">
+                        <a href="http://invite.teamspeak.com/evoque.ts3srv.ru" target="_blank" class="pl-3 pr-2"><i class="fab fa-teamspeak"></i></a>
+                    </div>
+                    <div class="teamspeak">
+                        <a href="https://truckersmp.com/vtc/11682" target="_blank" class="pl-3 pr-2"><i class="fas fa-truck-pickup"></i></a>
+                    </div>
+                </div>
             </div>
             <div class="col-md-3 col-sm-12">
                 <ul class="nav flex-column text-center">
@@ -32,21 +45,8 @@
                 </ul>
             </div>
             <div class="col-md-3 col-sm-12 row flex-column justify-content-center align-items-center ml-0">
-                <h2>Наши контакты</h2>
-                <div class="contacts row">
-                    <div class="vk">
-                        <a href="https://vk.com/evoquevtc" target="_blank" class="pl-3 pr-2"><i class="fab fa-vk"></i></a>
-                    </div>
-                    <div class="discord">
-                        <a href="https://discord.com/invite/Gj53a8d" target="_blank" class="pl-3 pr-2"><i class="fab fa-discord"></i></a>
-                    </div>
-                    <div class="teamspeak">
-                        <a href="http://invite.teamspeak.com/evoque.ts3srv.ru" target="_blank" class="pl-3 pr-2"><i class="fab fa-teamspeak"></i></a>
-                    </div>
-                    <div class="teamspeak">
-                        <a href="https://truckersmp.com/vtc/11682" target="_blank" class="pl-3 pr-2"><i class="fas fa-truck-pickup"></i></a>
-                    </div>
-                </div>
+                <h2>@lang('general.vtc_evoque')</h2>
+                <span class="copyright">Все права защищены &copy; {{ date('Y') }}</span>
             </div>
         </div>
     </div>

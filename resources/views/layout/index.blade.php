5fe4d9e6927692b6d696dd06e14d1398bb477217
Eugene <viiper94@gmail.com>
2020-11-29 11:35:28 +0200

    added open graph stuff

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -1,10 +1,11 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" xmlns:og="http://ogp.me/ns#">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @yield('meta')
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139146641-2"></script>
     <script>

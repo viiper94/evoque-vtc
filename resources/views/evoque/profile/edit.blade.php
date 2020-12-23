312676fe23304b615fa4a4d9d94e6a710c4f8e95
Eugene <viiper94@gmail.com>
2020-12-23 20:57:59 +0200

    added plate select, email and avatar

--- resources/views/evoque/profile/edit.blade.php
+++ resources/views/evoque/profile/edit.blade.php
@@ -12,48 +12,81 @@
 @section('content')
 
     <div class="container pt-5 pb-5">
+        @include('layout.alert')
         <h2 class="mt-3 text-primary">Редактирование профиля</h2>
         <form method="post">
             @csrf
-            <div class="form-group">
-                <label for="name">Имя и Фамилия</label>
-                <input type="text" class="form-control" name="name" id="name" value="{{ $user->name }}" required>
-                @if($errors->has('name'))
-                    <small class="form-text">{{ $errors->first('name') }}</small>
-                @endif
-            </div>
             <div class="row">
-                <div class="form-group col-sm-6">
-                    <label for="city">Город</label>
-                    <input type="text" class="form-control" name="city" id="city" value="{{ $user->city }}" required>
-                    @if($errors->has('city'))
-                        <small class="form-text">{{ $errors->first('city') }}</small>
-                    @endif
+                <div class="col-auto">
+                    <label>Аватар</label>
+                    <img src="{{ $user->image }}" alt="{{ $user->name }}" class="text-shadow-m d-block">
+                    <a href="{{ route('evoque.profile.updateAvatar') }}" class="btn btn-sm btn-outline-warning my-1"><i class="fab fa-steam"></i> Загрузить Steam аватар</a>
                 </div>
-                <div class="form-group col-sm-6">
-                    <label for="country">Страна</label>
-                    <input type="text" class="form-control" name="country" id="country" value="{{ $user->country }}" required>
-                    @if($errors->has('country'))
-                        <small class="form-text">{{ $errors->first('country') }}</small>
-                    @endif
+                <div class="col">
+                    <div class="form-group">
+                        <label for="name">Имя и Фамилия</label>
+                        <input type="text" class="form-control" name="name" id="name" value="{{ $user->name }}" required>
+                        @if($errors->has('name'))
+                            <small class="form-text">{{ $errors->first('name') }}</small>
+                        @endif
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-sm-6">
+                            <label for="city">Город</label>
+                            <input type="text" class="form-control" name="city" id="city" value="{{ $user->city }}" required>
+                            @if($errors->has('city'))
+                                <small class="form-text">{{ $errors->first('city') }}</small>
+                            @endif
+                        </div>
+                        <div class="form-group col-sm-6">
+                            <label for="country">Страна</label>
+                            <input type="text" class="form-control" name="country" id="country" value="{{ $user->country }}" required>
+                            @if($errors->has('country'))
+                                <small class="form-text">{{ $errors->first('country') }}</small>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="birth_date">Дата рождения</label>
+                        <input type="text" class="form-control" name="birth_date" id="birth_date"
+                               value="{{ $user->birth_date ? $user->birth_date->format('d.m.Y') : '' }}" autocomplete="off"
+                               placeholder="Укажите в формате дд.мм.гггг или выберите в календаре" required>
+                        @if($errors->has('birth_date'))
+                            <small class="form-text">{{ $errors->first('birth_date') }}</small>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label for="email">E-Mail</label>
+                        <input type="email" class="form-control" name="email" id="email" value="{{ $user->email }}">
+                        @if($errors->has('email'))
+                            <small class="form-text">{{ $errors->first('email') }}</small>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label for="vk">Ссылка ВК</label>
+                        <input type="url" class="form-control" name="vk" id="vk_link" value="{{ $user->vk }}">
+                        @if($errors->has('vk'))
+                            <small class="form-text">{{ $errors->first('vk') }}</small>
+                        @endif
+                    </div>
                 </div>
             </div>
-            <div class="form-group">
-                <label for="birth_date">Дата рождения</label>
-                <input type="text" class="form-control" name="birth_date" id="birth_date"
-                       value="{{ $user->birth_date ? $user->birth_date->format('d.m.Y') : '' }}" autocomplete="off"
-                       placeholder="Укажите в формате дд.мм.гггг или выберите в календаре" required>
-                @if($errors->has('birth_date'))
-                    <small class="form-text">{{ $errors->first('birth_date') }}</small>
-                @endif
-            </div>
-            <div class="form-group">
-                <label for="vk">Ссылка ВК</label>
-                <input type="url" class="form-control" name="vk" id="vk_link" value="{{ $user->vk }}">
-                @if($errors->has('vk'))
-                    <small class="form-text">{{ $errors->first('vk') }}</small>
-                @endif
-            </div>
+
+            @if($user->member)
+                <div class="form-group">
+                    <label for="plate">Номерной знак</label>
+                    <div class="row m-0">
+                        <input type="text" class="form-control col" name="plate" id="plate" value="{{ $user->member->plate }}"
+                               maxlength="3" placeholder="Три цифры" data-token="{{ csrf_token() }}">
+                        @if($user->member->plate)
+                            <div class="col-auto pr-0">
+                                <img src="/images/plates/{{ $user->member->plate }}.png" id="plate-img" style="height: 38px;">
+                            </div>
+                        @endif
+                    </div>
+                    <small class="form-text">@if($errors->has('vk')){{ $errors->first('vk') }}@endif</small>
+                </div>
+            @endif
             <div class="row text-center justify-content-center">
                 <button type="submit" class="btn btn-outline-warning btn-lg">Сохранить</button>
             </div>

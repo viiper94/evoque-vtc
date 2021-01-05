0f447f035c5b6394ec0a8e99ba4c518f26ed7768
Eugene <viiper94@gmail.com>
2021-01-05 11:38:00 +0200

    added blade @markdown directive

---
+++ app/Providers/BladeServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class BladeServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Blade::directive('markdown', function ($expression) {
+            return "<?php echo Parsedown::instance()->setBreaksEnabled(true)->setSafeMode(true)->text($expression); ?>";
+        });
+    }
+}

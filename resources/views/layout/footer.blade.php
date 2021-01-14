d2f44cf45904282ff9b380fb498ba4af5b96b1b8
Eugene <viiper94@gmail.com>
2021-01-14 20:34:18 +0200

    changed footer grid

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,24 +1,17 @@
 <footer>
-    <div class="container pt-5 pb-5 pl-5 pr-5">
-        <div class="row">
-            <div class="col-md-3 col-sm-12 row flex-column justify-content-center align-items-center ml-0">
+    <div class="container py-5">
+        <div class="row justify-content-between">
+            <div class="col-md-auto col-sm-12 row flex-column justify-content-center align-items-center ml-0">
                 <h2>Наши контакты</h2>
-                <div class="contacts row">
-                    <div class="vk">
-                        <a href="https://vk.com/evoquevtc" target="_blank" class="pl-3 pr-2"><i class="fab fa-vk"></i></a>
-                    </div>
-                    <div class="discord">
-                        <a href="https://discord.gg/Gj53a8d" target="_blank" class="pl-3 pr-2"><i class="fab fa-discord"></i></a>
-                    </div>
-                    <div class="teamspeak">
-                        <a href="http://invite.teamspeak.com/evoque.ts3srv.ru" target="_blank" class="pl-3 pr-2"><i class="fab fa-teamspeak"></i></a>
-                    </div>
-                    <div class="teamspeak">
-                        <a href="https://truckersmp.com/vtc/11682" target="_blank" class="pl-3 pr-2"><i class="fas fa-truck-pickup"></i></a>
-                    </div>
+                <div class="contacts row mx-0 justify-content-between flex-nowrap w-100">
+                    <a href="https://vk.com/evoquevtc" target="_blank" class=""><i class="fab fa-vk"></i></a>
+                    <a href="https://discord.gg/Gj53a8d" target="_blank" class=""><i class="fab fa-discord"></i></a>
+                    <a href="http://invite.teamspeak.com/evoque.ts3srv.ru" target="_blank" class=""><i class="fab fa-teamspeak"></i></a>
+                    <a href="https://truckersmp.com/vtc/11682" target="_blank" class=""><i class="fas fa-truck-pickup"></i></a>
+                    <a href="https://trucksbook.eu/company/11661" target="_blank" class=""><i class="fontello icon-trucksbook"></i></a>
                 </div>
             </div>
-            <div class="col-md-3 col-sm-12">
+            <div class="col-md-auto col-sm-12">
                 <ul class="nav flex-column text-center">
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('apply') }}">Вступить</a>
@@ -38,7 +31,7 @@
                     </li>
                 </ul>
             </div>
-            <div class="col-md-3 col-sm-12">
+            <div class="col-md-auto col-sm-12">
                 <ul class="nav flex-column text-center">
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('members') }}">Команда</a>
@@ -54,7 +47,7 @@
                     </li>
                 </ul>
             </div>
-            <div class="col-md-3 col-sm-12 row flex-column justify-content-center align-items-center ml-0">
+            <div class="col-md-auto col-sm-12 row flex-column justify-content-center align-items-center ml-0">
                 <h2>@lang('general.vtc_evoque')</h2>
                 <span class="copyright">Все права защищены &copy; {{ date('Y') }}</span>
             </div>

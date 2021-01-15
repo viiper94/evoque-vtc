8ab084ef6889b3a0b8974687039471035365583a
Eugene <viiper94@gmail.com>
2021-01-15 23:27:40 +0200

    fixed footer responsive

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,9 +1,9 @@
 <footer>
     <div class="container py-5">
-        <div class="row justify-content-between">
-            <div class="col-md-auto col-sm-12 row flex-column justify-content-center align-items-center ml-0">
+        <div class="row justify-content-center">
+            <div class="col-auto col-md-6 col-xl-auto mb-md-0 contacts">
                 <h2>Наши контакты</h2>
-                <div class="contacts row mx-0 justify-content-between flex-nowrap w-100">
+                <div class="contacts-links mx-0">
                     <a href="https://vk.com/evoquevtc" target="_blank" class=""><i class="fab fa-vk"></i></a>
                     <a href="https://discord.gg/Gj53a8d" target="_blank" class=""><i class="fab fa-discord"></i></a>
                     <a href="http://invite.teamspeak.com/evoque.ts3srv.ru" target="_blank" class=""><i class="fab fa-teamspeak"></i></a>
@@ -11,8 +11,8 @@
                     <a href="https://trucksbook.eu/company/11661" target="_blank" class=""><i class="fontello icon-trucksbook"></i></a>
                 </div>
             </div>
-            <div class="col-md-auto col-sm-12">
-                <ul class="nav flex-column text-center">
+            <div class="col-md-6 col-sm-12 col-xl-auto mb-md-3">
+                <ul class="nav flex-column text-center text-md-right">
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('apply') }}">Вступить</a>
                     </li>
@@ -31,8 +31,8 @@
                     </li>
                 </ul>
             </div>
-            <div class="col-md-auto col-sm-12">
-                <ul class="nav flex-column text-center">
+            <div class="col-md-6 col-sm-12 col-xl-auto mb-md-3">
+                <ul class="nav flex-column text-center text-md-left">
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('members') }}">Команда</a>
                     </li>
@@ -47,9 +47,9 @@
                     </li>
                 </ul>
             </div>
-            <div class="col-md-auto col-sm-12 row flex-column justify-content-center align-items-center ml-0">
+            <div class="col-xl-auto col-md-6 col-sm-12 copyright">
                 <h2>@lang('general.vtc_evoque')</h2>
-                <span class="copyright">Все права защищены &copy; {{ date('Y') }}</span>
+                <span class="text-muted">Все права защищены &copy; {{ date('Y') }}</span>
             </div>
         </div>
     </div>

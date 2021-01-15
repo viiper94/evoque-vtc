49cae23dae4d858ea6d2d9d0b1b9cbe2045d1b0a
Eugene <viiper94@gmail.com>
2021-01-15 20:35:08 +0200

    not showing gallery badge if 0 invisible photos

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -18,7 +18,7 @@
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('gallery') }}">Галлерея
-                            @if(\Illuminate\Support\Facades\Auth::check() && \Illuminate\Support\Facades\Auth::user()->can('toggle', \App\Gallery::class))
+                            @if(\Illuminate\Support\Facades\Auth::check() && \Illuminate\Support\Facades\Auth::user()->can('toggle', \App\Gallery::class) && $gallery_c > 0)
                                 <span class="badge badge-danger">{{ $gallery_c }}</span>
                             @endif
                         </a>

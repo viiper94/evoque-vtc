894f0bddee382b18ee06e5267c99e673e28b9b80
Eugene <viiper94@gmail.com>
2021-02-07 21:26:03 +0200

    added litepicker to tab page

--- resources/views/evoque/convoys/tab/edit.blade.php
+++ resources/views/evoque/convoys/tab/edit.blade.php
@@ -25,29 +25,36 @@
         @endif
         <form method="post" enctype="multipart/form-data">
             @csrf
-            <div class="form-group">
-                <label for="convoy_title">Название конвоя</label>
-                <input type="text" class="form-control" id="convoy_title" name="convoy_title" value="{{ $tab->convoy_title }}" required>
-                @if($errors->has('convoy_title'))
-                    <small class="form-text">{{ $errors->first('convoy_title') }}</small>
-                @endif
-            </div>
-            <div class="form-group">
-                <label for="date">Дата конвоя</label>
-                <input type="text" class="form-control" name="date" id="date" value="{{ $tab->date ? $tab->date->format('d.m.Y') : '' }}" autocomplete="off" required>
-                @if($errors->has('date'))
-                    <small class="form-text">{{ $errors->first('date') }}</small>
-                @endif
-            </div>
-            <div class="form-group">
-                <label for="lead_id">Ведущий</label>
-                <select class="form-control" id="lead_id" name="lead_id" required>
-                    @foreach($members as $member)
-                        <option value="{{ $member->id }}" @if($member->nickname === \Illuminate\Support\Facades\Auth::user()->member->nickname) selected @endif >
-                            {{ $member->nickname }}
-                        </option>
-                    @endforeach
-                </select>
+            <div class="row">
+                <div class="col">
+                    <div class="form-group">
+                        <label for="convoy_title">Название конвоя</label>
+                        <input type="text" class="form-control" id="convoy_title" name="convoy_title" value="{{ $tab->convoy_title }}" required>
+                        @if($errors->has('convoy_title'))
+                            <small class="form-text">{{ $errors->first('convoy_title') }}</small>
+                        @endif
+                    </div>
+
+                    <div class="form-group">
+                        <label for="lead_id">Ведущий</label>
+                        <select class="form-control" id="lead_id" name="lead_id" required>
+                            @foreach($members as $member)
+                                <option value="{{ $member->id }}" @if($member->nickname === \Illuminate\Support\Facades\Auth::user()->member->nickname) selected @endif >
+                                    {{ $member->nickname }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="form-group">
+                        <label for="date">Дата конвоя</label><br>
+                        <input type="hidden" name="date" id="date" value="{{ $tab->date ? $tab->date->format('d.m.Y') : '' }}" required>
+                        @if($errors->has('date'))
+                            <small class="form-text">{{ $errors->first('date') }}</small>
+                        @endif
+                    </div>
+                </div>
             </div>
             <div class="form-group">
                 <label for="description">Дополнительная информация</label>
@@ -61,7 +68,7 @@
                     <input type="file" class="custom-file-input uploader" id="screenshot" name="screenshot" accept="image/*">
                     <label class="custom-file-label" for="screenshot">Выберите скрин</label>
                 </div>
-                <img src="/images/convoys/tab/{{ $tab->screenshot ?? 'image-placeholder.jpg' }}" alt="Скрин TAB" class="w-100" id="screenshot-preview">
+                <img src="{{ $tab->screenshot ? '/images/convoys/tab/'.$tab->screenshot : '' }}" class="w-100" id="screenshot-preview">
             </div>
             <div class="row justify-content-center">
                 <button class="btn btn-outline-warning btn-lg" type="submit"><i class="fas fa-save"></i> Сохранить</button>
@@ -70,27 +77,13 @@
     </div>
 
     <script>
-        $('#date').datetimepicker({
-            i18n:{
-                ru:{
-                    months:[
-                        'Январь','Февраль','Март','Апрель',
-                        'Май','Июнь','Июль','Август',
-                        'Сентябрь','Октябрь','Ноябрь','Декабрь',
-                    ],
-                    dayOfWeek:[
-                        "Вс", "Пн", "Вт", "Ср",
-                        "Чт", "Пт", "Сб",
-                    ]
-                }
-            },
-            format: 'd.m.Y',
-            theme: 'dark',
-            dayOfWeekStart: '1',
-            timepicker: false,
-            scrollInput: false
+        const picker = new Litepicker({
+            element: document.getElementById('date'),
+            lang: 'ru-RU',
+            inlineMode: true,
+            startDate: Date.now(),
+            format: 'DD.MM.YYYY'
         });
-        $.datetimepicker.setLocale('ru');
     </script>
 
 @endsection

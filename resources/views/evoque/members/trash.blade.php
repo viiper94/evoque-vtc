7b30d3f1c3db71fdf66fb13560362f2dae70d965
Eugene <viiper94@gmail.com>
2021-03-25 11:24:00 +0200

    added pagination and search

--- resources/views/evoque/members/trash.blade.php
+++ resources/views/evoque/members/trash.blade.php
@@ -9,6 +9,17 @@
     <div class="container-fluid pt-5">
         @include('layout.alert')
         <h2 class="mt-3 text-center text-primary">Уволенные сотрудники</h2>
+        <div class="row justify-content-between">
+            <form method="get" class="col">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Поиск по никнейму" name="q" style="max-width: 400px">
+                </div>
+            </form>
+            <div class="col-auto mr-3">
+                {{ $members->links('layout.pagination') }}
+            </div>
+        </div>
+
         <div class="table-responsive mb-5">
             <table class="users table table-dark table-hover text-center"><thead>
                 <tr>
@@ -20,43 +31,23 @@
                     <th scope="col">В компании с</th>
                     <th scope="col">На сайте с</th>
                     <th scope="col">Уволен</th>
+                    <th scope="col">С восстановлением</th>
                     <th scope="col">#</th>
                 </tr>
                 </thead>
                 <tbody>
-                @foreach($members as $user)
+                @foreach($members as $member)
                     <tr>
-                        <td class="user-image"><img src="{{ $user->image }}"></td>
-                        <td><a href="{{ route('evoque.profile', $user->id) }}">{{ $user->name }}</td>
-                        <td><a href="{{ $user->vk }}" target="_blank">{{ $user->vk }}</a></td>
-                        <td><a href="https://steamcommunity.com/profiles/{{ $user->steamid64 }}" target="_blank">{{ $user->steamid64 }}</a></td>
-                        <td><a href="https://truckersmp.com/user/{{ $user->truckersmp_id }}" target="_blank">{{ $user->truckersmp_id }}</a></td>
-                        <td>
-                            @if($user->member)
-                                @if($user->member->visible)
-                                    @can('update', \App\Member::class)
-                                        <a href="{{ route('evoque.admin.members.edit', $user->member->id) }}"><i class="fontello icon-evoque text-primary"></i></a>
-                                    @else
-                                        <i class="fas fa-check"></i>
-                                    @endcan
-                                @else
-                                    @can('update', \App\Member::class)
-                                        <a href="{{ route('evoque.admin.members.edit', $user->member->id) }}"><i class="fontello icon-evoque text-danger"></i></a>
-                                    @else
-                                        <i class="fas fa-times text-danger"></i>
-                                    @endcan
-                                @endif
-                            @else
-                                <i class="fas fa-times text-danger"></i>
-                            @endif
-                        </td>
-                        <td>{{ $user->created_at->isoFormat('DD.MM.Y HH:mm') }}</td>
-                        <td>{{ !$user->fired_at ? '-' : $user->fired_at->isoFormat('DD.MM.Y HH:mm') }}</td>
-                        <td>
-                            @can('setAsMember', $user)
-                                <a href="{{ route('evoque.admin.users.setAsMember', $user->id) }}" onclick="return confirm('Восстановить сотрудника?')"><i class="fas fa-undo"></i></a>
-                            @endif
-                        </td>
+                        <td><a href="{{ route('evoque.admin.members.edit', $member->id) }}">{{ $member->nickname }}</td>
+                        <td><a href="{{ route('evoque.profile', $member->user->id) }}">{{ $member->user->name }}</td>
+                        <td><a href="{{ $member->user->vk }}" target="_blank">{{ $member->user->vk }}</a></td>
+                        <td><a href="https://steamcommunity.com/profiles/{{ $member->user->steamid64 }}" target="_blank">{{ $member->user->steamid64 }}</a></td>
+                        <td><a href="https://truckersmp.com/user/{{ $member->user->truckersmp_id }}" target="_blank">{{ $member->user->truckersmp_id }}</a></td>
+                        <td>{{ $member->join_date->isoFormat('DD.MM.Y') }}</td>
+                        <td>{{ $member->user->created_at->isoFormat('DD.MM.Y HH:mm') }}</td>
+                        <td>{{ $member->deleted_at->isoFormat('DD.MM.Y HH:mm') }}</td>
+                        <td></td>
+                        <td><a href="{{ route('evoque.admin.members.restore', $member->id) }}" onclick="return confirm('Восстановить сотрудника?')"><i class="fas fa-undo"></i></a></td>
                     </tr>
                 @endforeach
                 </tbody>

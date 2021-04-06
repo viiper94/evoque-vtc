9188d7ba7a653d98e71b7442f1c52e916b7ab7fb
Eugene <viiper94@gmail.com>
2021-04-06 16:39:29 +0300

    added text if no reports

--- resources/views/evoque/rp/reports.blade.php
+++ resources/views/evoque/rp/reports.blade.php
@@ -26,80 +26,86 @@
     </div>
     <div class="container-fluid">
         <div class="rp-reports pt-3 pb-5 row justify-content-around align-items-baseline">
-            @foreach($reports as $report)
-                <div class="card card-dark col-12 col-md-auto text-shadow-m m-3 p-0
+            @if(count($reports) > 0)
+                @foreach($reports as $report)
+                    <div class="card card-dark col-12 col-md-auto text-shadow-m m-3 p-0
                         @if($report->status === 2) border-danger
                         @elseif($report->status === 1) border-success
                         @else border-primary @endif">
-                    <div class="card-header row mx-0">
-                        <div class="col row rp-title">
-                            <h5 class="mb-0">
-                                @if($report->status == '0')
-                                    <i class="fas fa-arrow-alt-circle-up text-warning"></i>
-                                @elseif($report->status == '1')
-                                    <i class="fas fa-check-circle text-success"></i>
-                                @elseif($report->status == '2')
-                                    <i class="fas fa-times-circle text-danger"></i>
-                                @endif
-                                @can('decline', $report)
-                                    <a href="{{ route('evoque.rp.reports.view', $report->id) }}">
-                                @endcan
-                                @if($report->member)
-                                    От {{ $report->member->nickname }}
-                                @else
-                                    <span class="no-member">Отчёт уволенного сотрудника</span>
-                                @endif
-                                @can('decline', $report)
-                                    </a>
-                                @endcan
-                            </h5>
-                        </div>
-                        <h5 class="col-auto text-md-right text-muted mb-0">{{ $report->game === 'ets2' ? 'ETS2' : 'ATS' }}</h5>
-                        @if(\Illuminate\Support\Facades\Auth::user()->can('decline', $report) ||
-                                    \Illuminate\Support\Facades\Auth::user()->can('delete', $report))
-                            <div class="dropdown dropdown-dark col-auto px-0 dropleft">
-                                <button class="btn dropdown-toggle no-arrow py-0" type="button" id="dropdownMenuButton"
-                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="fas fa-ellipsis-v"></i>
-                                </button>
-                                <div class="dropdown-menu text-shadow-m" aria-labelledby="dropdownMenuButton">
+                        <div class="card-header row mx-0">
+                            <div class="col row rp-title">
+                                <h5 class="mb-0">
+                                    @if($report->status == '0')
+                                        <i class="fas fa-arrow-alt-circle-up text-warning"></i>
+                                    @elseif($report->status == '1')
+                                        <i class="fas fa-check-circle text-success"></i>
+                                    @elseif($report->status == '2')
+                                        <i class="fas fa-times-circle text-danger"></i>
+                                    @endif
                                     @can('decline', $report)
-                                        <a href="{{ route('evoque.rp.reports.view', $report->id) }}" class="dropdown-item"><i class="fas fa-eye"></i> Смотреть</a>
+                                        <a href="{{ route('evoque.rp.reports.view', $report->id) }}">
+                                            @endcan
+                                            @if($report->member)
+                                                От {{ $report->member->nickname }}
+                                            @else
+                                                <span class="no-member">Отчёт уволенного сотрудника</span>
+                                            @endif
+                                            @can('decline', $report)
+                                        </a>
                                     @endcan
-                                    @can('delete', $report)
-                                        <a href="{{ route('evoque.rp.reports.delete', $report->id) }}"
-                                           class="dropdown-item" onclick="return confirm('Удалить этот отчёт?')"><i class="fas fa-trash"></i> Удалить</a>
-                                    @endcan
+                                </h5>
+                            </div>
+                            <h5 class="col-auto text-md-right text-muted mb-0">{{ $report->game === 'ets2' ? 'ETS2' : 'ATS' }}</h5>
+                            @if(\Illuminate\Support\Facades\Auth::user()->can('decline', $report) ||
+                                        \Illuminate\Support\Facades\Auth::user()->can('delete', $report))
+                                <div class="dropdown dropdown-dark col-auto px-0 dropleft">
+                                    <button class="btn dropdown-toggle no-arrow py-0" type="button" id="dropdownMenuButton"
+                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fas fa-ellipsis-v"></i>
+                                    </button>
+                                    <div class="dropdown-menu text-shadow-m" aria-labelledby="dropdownMenuButton">
+                                        @can('decline', $report)
+                                            <a href="{{ route('evoque.rp.reports.view', $report->id) }}" class="dropdown-item"><i class="fas fa-eye"></i> Смотреть</a>
+                                        @endcan
+                                        @can('delete', $report)
+                                            <a href="{{ route('evoque.rp.reports.delete', $report->id) }}"
+                                               class="dropdown-item" onclick="return confirm('Удалить этот отчёт?')"><i class="fas fa-trash"></i> Удалить</a>
+                                        @endcan
+                                    </div>
                                 </div>
+                            @endif
+                        </div>
+                        <div class="card-body">
+                            @if($report->note)
+                                <p class="font-weight-bold text-primary mb-0">Дополнительная информация:</p>
+                                <div class="markdown-content">
+                                    @markdown($report->note)
+                                </div>
+                            @endif
+                            @if($report->comment)
+                                <p class="text-danger mb-0">Комментарий от {{ $report->comment_by }}:</p>
+                                <div class="markdown-content">
+                                    @markdown($report->comment)
+                                </div>
+                            @endif
+                        </div>
+                        <div class="row mx-0">
+                            <div class="fotorama" data-allowfullscreen="true" data-maxheight="600">
+                                @foreach($report->images as $image)
+                                    <img src="/images/rp/{{ $image }}" style="max-width: 100%">
+                                @endforeach
                             </div>
-                        @endif
-                    </div>
-                    <div class="card-body">
-                        @if($report->note)
-                            <p class="font-weight-bold text-primary mb-0">Дополнительная информация:</p>
-                            <div class="markdown-content">
-                                @markdown($report->note)
-                            </div>
-                        @endif
-                        @if($report->comment)
-                            <p class="text-danger mb-0">Комментарий от {{ $report->comment_by }}:</p>
-                            <div class="markdown-content">
-                                @markdown($report->comment)
-                            </div>
-                        @endif
-                    </div>
-                    <div class="row mx-0">
-                        <div class="fotorama" data-allowfullscreen="true" data-maxheight="600">
-                            @foreach($report->images as $image)
-                                <img src="/images/rp/{{ $image }}" style="max-width: 100%">
-                            @endforeach
+                        </div>
+                        <div class="card-footer text-muted">
+                            {{ $report->created_at->isoFormat('LLL') }}
                         </div>
                     </div>
-                    <div class="card-footer text-muted">
-                        {{ $report->created_at->isoFormat('LLL') }}
-                    </div>
-                </div>
-            @endforeach
+                @endforeach
+            @else
+                <h5 class="text-center">
+                    Еще не создано отчётов
+                </h5>
+            @endif
         </div>
         {{ $reports->links('layout.pagination') }}
         <div class="row">

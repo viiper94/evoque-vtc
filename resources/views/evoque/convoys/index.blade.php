a67f9b648c762cbb5cdd308d48078effeb22b75d
Eugene <viiper94@gmail.com>
2021-04-11 22:51:25 +0300

    reskining and merging private convoys page with admin convoys

--- resources/views/evoque/convoys/index.blade.php
+++ resources/views/evoque/convoys/index.blade.php
@@ -4,8 +4,13 @@
         Все конвои | @lang('general.vtc_evoque')
 @endsection
 
+@section('assets')
+    <link rel="stylesheet" type="text/css" href="/js/fotorama-4.6.4/fotorama.css">
+    <script src="/js/fotorama-4.6.4/fotorama.js"></script>
+@endsection
+
 @section('content')
-    <div class="container pt-5 members-table">
+    <div class="container-fluid pt-5">
         @include('layout.alert')
         <h1 class="mt-3 text-primary ml-3 text-center">
             Все конвои
@@ -15,25 +20,19 @@
         </h1>
         <div class="convoys mt-3 mb-5">
             @foreach($convoys as $day => $convoys_per_day)
-                <div class="row flex-column flex-md-row">
+                <div class="row flex-column flex-md-row mx-0">
                     <h1 class="text-primary text-md-right text-center col-md-auto mt-md-5 mt-3 p-0 convoys-day">{{ $day }}</h1>
-                    <div class="row col-md convoys-list m-0 p-0 my-0 my-md-3 mx-md-5 px-md-0">
+                    <div class="row col-md convoys-list m-0 p-0 my-0 my-md-3 mx-md-5 px-md-0 accordion" id="convoysAccordion-{{ $loop->index }}">
                         @foreach($convoys_per_day as $convoy)
                             <div class="card card-dark text-shadow-m my-md-2 ml-md-5 col-12 px-md-0
                                 @if($convoy->public && $convoy->isUpcoming()) border-primary
                                 @elseif(($convoy->booking && !$convoy->visible) || !$convoy->start_city) border-danger @endif
                                 @if($convoy->isUpcoming())upcoming @endif">
-                                <h6 class="card-header row mb-0 mx-0">
+                                <div class="card-header row mb-0 mx-0" id="convoy-{{ $convoy->id }}-header"
+                                     data-toggle="collapse" data-target="#convoy-{{ $convoy->id }}-info"
+                                     aria-expanded="false" aria-controls="convoy-{{ $convoy->id }}-info">
                                     <div class="col px-0 app-title">
-                                        <span class="text-muted">
-                                            @if(!$convoy->visible)
-                                                <i class="fas fa-eye-slash text-warning"></i>
-                                            @else
-                                                <i class="fas fa-eye text-success"></i>
-                                            @endif
-                                            {{ $convoy->getType() }}
-                                        </span>
-                                        <a href="{{ route('evoque.admin.convoy.edit', $convoy->id) }}">{{ $convoy->title }}</a>
+                                        <h5 class="m-auto text-center">{{ $convoy->start_time->format('H:i') }} - {{ $convoy->title }}</h5>
                                     </div>
                                     @can('update', \App\Convoy::class)
                                         <div class="dropdown dropdown-dark col-auto px-0 dropleft">
@@ -55,14 +54,34 @@
                                             </div>
                                         </div>
                                     @endcan
-                                </h6>
-                                <div class="card-body">
+                                </div>
+                                <div class="card-header">
+                                        <span class="text-muted">
+                                            @if(!$convoy->visible)
+                                                <i class="fas fa-eye-slash text-warning"></i>
+                                            @else
+                                                <i class="fas fa-eye text-success"></i>
+                                            @endif
+                                            {{ $convoy->getType() }}
+                                        </span>
                                     <p class="card-text">
                                         Ведущий: <b>{{ $convoy->lead }}</b>
                                         @if($convoy->leadMember && $convoy->leadMember->user->vk)
                                             <a href="{{ $convoy->leadMember->user->vk }}" target="_blank"><i class="fab fa-vk"></i></a>
                                         @endif
                                     </p>
+                                </div>
+                                <div class="card-body collapse" id="convoy-{{ $convoy->id }}-info" aria-labelledby="convoy-{{ $convoy->id }}-header" data-parent="#convoysAccordion-{{ $loop->parent->index }}">
+                                    @if($convoy->route)
+                                        <section class="w-100">
+                                            <h1 class="text-center">Маршрут конвоя</h1>
+                                            <div class="fotorama" data-allowfullscreen="true" data-nav="thumbs">
+                                                @foreach($convoy->route as $item)
+                                                    <img src="/images/convoys/{{ $item }}">
+                                                @endforeach
+                                            </div>
+                                        </section>
+                                    @endif
                                 </div>
                             </div>
                         @endforeach

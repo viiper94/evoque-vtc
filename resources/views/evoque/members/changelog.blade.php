dbc542bcc9f9c4222900dd2f0d4ab8b1da51bb0e
Eugene <viiper94@gmail.com>
2021-04-28 15:34:45 +0300

    fixed member changelog container

--- resources/views/evoque/members/changelog.blade.php
+++ resources/views/evoque/members/changelog.blade.php
@@ -6,6 +6,8 @@
 
 @section('content')
 
-    @include('layout.changelog', ['items' => $member->audits])
+    <div class="container pt-5">
+        @include('layout.changelog', ['items' => $member->audits])
+    </div>
 
 @endsection

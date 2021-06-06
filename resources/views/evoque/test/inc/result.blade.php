353ac802c770ed4a308e9da6034e80fce3b48b1b
Eugene <viiper94@gmail.com>
2021-06-06 10:08:35 +0300

    added no questions text

--- resources/views/evoque/test/inc/result.blade.php
+++ resources/views/evoque/test/inc/result.blade.php
@@ -1,6 +1,10 @@
 <div class="row my-5 py-5 flex-column align-items-center">
-    <h2>Тест пройден!</h2>
-    <p class="text-muted">Следующая попытка будет открыта через 1 месяц</p>
-    <h3 class="my-5">Ваш результат {{ count($correct) }}/{{ $count }}</h3>
-    <a href="{{ route('evoque.test', 1) }}" class="btn btn-sm btn-primary m-auto">Смотреть ответы</a>
+    @if($count > 0)
+        <h2>Тест пройден!</h2>
+        <p class="text-muted">Следующая попытка будет открыта через 1 месяц</p>
+        <h3 class="my-5">Ваш результат {{ count($correct) }}/{{ $count }}</h3>
+        <a href="{{ route('evoque.test', 1) }}" class="btn btn-sm btn-primary m-auto">Смотреть ответы</a>
+    @else
+        <h2>Еще нет вопросов!</h2>
+    @endif
 </div>

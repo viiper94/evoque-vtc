98991e3b62581cf99235d3d6abb28382ceb8d92c
Eugene <viiper94@gmail.com>
2021-09-21 12:29:29 +0300

    added wyoming dlc to rewards list

--- app/RpStats.php
+++ app/RpStats.php
@@ -25,8 +25,8 @@ class RpStats extends Model{
             60000 => 'Любая раскраска (на ваш выбор), Steering Creations Pack + 4 балла',
             110000 => 'Wheel Tuning Pack, Goodyear Tires Pack + 5 баллов',
             170000 => 'Heavy Cargo Pack, Special Transport, Forest Machinery + 6 баллов',
-            240000 => 'New Mexico, Oregon, Washington + 7 баллов',
-            450000 => 'Utah, Idaho, Colorado + 8 баллов',
+            240000 => 'New Mexico, Oregon, Washington, Utah + 7 баллов',
+            450000 => 'Idaho, Colorado, Wyoming + 8 баллов',
             700000 => 'Любая игра или DLC в Steam стоимостью до 1000 рублей + 9 баллов',
         ],
     ];

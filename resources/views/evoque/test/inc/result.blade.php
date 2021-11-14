7db4374fcff416e8238cb7a6600a8f0164bd0c96
Eugene <viiper94@gmail.com>
2021-11-14 23:14:16 +0200

    fixed text

--- resources/views/evoque/test/inc/result.blade.php
+++ resources/views/evoque/test/inc/result.blade.php
@@ -1,7 +1,8 @@
 <div class="row my-5 py-5 flex-column align-items-center">
     @if($count > 0)
         <h2>Вы ответили на все вопросы!</h2>
-        <p class="text-muted">Через {{ $first_answered->created_at->addDays(31)->diffInDays(\Carbon\Carbon::now()) }} дней ваши результаты будут обнулены.</p>
+        <p class="text-muted">Через {{ $first_answered->created_at->addDays(31)->diffInDays(\Carbon\Carbon::now()) }}
+            {{ trans_choice('день|дня|дней', $first_answered->created_at->addDays(31)->diffInDays(\Carbon\Carbon::now())) }} ваши результаты будут обнулены.</p>
         <h3 class="my-5">Результат {{ count($correct) }}/{{ $count }}</h3>
         <a href="{{ route('evoque.test', 1) }}" class="btn btn-sm btn-primary m-auto">Смотреть ответы</a>
     @else

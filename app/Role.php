79108e36ae3d1e276e293aa1530090fb20a32b3f
Eugene <viiper94@gmail.com>
2021-11-18 11:20:35 +0200

    roles attributes improvements

--- app/Role.php
+++ app/Role.php
@@ -14,6 +14,108 @@ class Role extends Model{
         'max_scores'
     ];
 
+    public static $permission_list = [
+        'members' => [
+            'manage_members',
+            'edit_members',
+//            'edit_members_activity',
+            'edit_members_rp_stats',
+            'fire_members',
+            'set_members_activity',
+            'reset_members_activity',
+            'see_bans',
+        ],
+        'applications' => [
+            'manage_applications',
+            'view_recruitments',
+            'claim_recruitments',
+            'delete_recruitments',
+            'make_applications',
+            'view_applications',
+            'claim_applications',
+            'delete_applications',
+        ],
+        'convoy' => [
+            'manage_convoys',
+            'view_all_convoys',
+            'book_convoys',
+            'quick_book_convoys',
+            'add_convoys',
+            'edit_convoys',
+            'delete_convoys',
+        ],
+        'tab' => [
+            'manage_tab',
+            'view_tab',
+//            'add_tab',
+            'edit_tab',
+            'accept_tab',
+            'delete_tab',
+        ],
+        'rp' => [
+            'manage_rp',
+            'add_reports',
+            'view_all_reports',
+            'delete_own_reports',
+            'delete_all_reports',
+            'accept_reports',
+            'reset_stats',
+        ],
+        'rules' => [
+            'manage_rules',
+            'add_rules',
+            'edit_rules',
+            'delete_rules',
+            'view_rules_changelog',
+        ],
+        'roles' => [
+            'manage_roles',
+            'view_roles',
+            'add_roles',
+            'edit_roles',
+            'edit_roles_permissions',
+            'delete_roles',
+        ],
+        'kb' => [
+            'manage_kb',
+            'view_private',
+            'view_hidden',
+            'add_article',
+            'edit_own_article',
+            'edit_all_articles',
+            'delete_own_article',
+            'delete_all_articles',
+        ],
+        'users' => [
+            'manage_users',
+            'view_users',
+            'set_user_as_member',
+        ],
+        'gallery' => [
+            'manage_gallery',
+            'upload_screenshots',
+            'upload_without_moderation',
+            'toggle_visibility',
+            'delete_screenshots',
+        ],
+        'test' => [
+            'manage_test',
+            'add_questions',
+            'edit_questions',
+            'delete_questions',
+            'view_results',
+            'delete_results',
+            'do_test',
+        ],
+        'tunings' => [
+            'manage_tunings',
+            'add_tunings',
+            'edit_tunings',
+            'delete_tunings',
+            'view_tunings',
+        ],
+    ];
+
     public function members(){
         return $this->belongsToMany('App\Member', 'role_member');
     }

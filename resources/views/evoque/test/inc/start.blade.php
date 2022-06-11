db76ed707c7aae40016a4be464e75f31a4e6a100
Eugene <viiper94@gmail.com>
2022-06-11 13:18:37 +0300

    added results count and some extra wording

--- resources/views/evoque/test/inc/start.blade.php
+++ resources/views/evoque/test/inc/start.blade.php
@@ -1,3 +1,5 @@
-<div class="row my-5 py-5">
-    <a href="{{ route('evoque.test', 1) }}" class="btn btn-lg btn-primary m-auto">Начать тест</a>
+<div class="row my-5 flex-column text-center">
+    <h4>Пройдено вопросов: {{ count($results) }}/{{ $count }}</h4>
+    <h6 class="text-muted mb-5">Для отображения результатов пройдите все вопросы</h6>
+    <a href="{{ route('evoque.test', 1) }}" class="btn btn-lg btn-primary m-auto">@if(count($results) > 0)Продолжить@elseНачать@endif тест</a>
 </div>

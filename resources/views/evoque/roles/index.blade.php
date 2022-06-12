e40406017650f4aa1ba667b2953a164812fb3ca1
Eugene <viiper94@gmail.com>
2022-06-12 11:12:16 +0300

    replaced button, removed modal

--- resources/views/evoque/roles/index.blade.php
+++ resources/views/evoque/roles/index.blade.php
@@ -18,7 +18,11 @@
                     <th scope="col">Группа</th>
                     <th scope="col" class="text-center">Участников</th>
                     <th scope="col"></th>
-                    <th scope="col"></th>
+                    <th scope="col">
+                        @can('create', \App\Role::class)
+                            <a href="{{ route('evoque.admin.roles.add') }}"><i class="fas fa-plus"></i></a>
+                        @endcan
+                    </th>
                 </tr>
                 </thead>
                 <tbody>
@@ -45,49 +49,6 @@
                 </tbody>
             </table>
         </div>
-        @can('create', \App\Role::class)
-            <div class="row justify-content-center">
-                <a data-toggle="modal" data-target="#role-modal" class="btn btn-outline-warning"><i class="fas fa-plus-square"></i> Новая роль</a>
-            </div>
-        @endcan
     </div>
-
-    @can('create', \App\Role::class)
-        <!-- Add role modal -->
-        <div class="modal fade" id="role-modal" tabindex="-1" aria-labelledby="Create role" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
-                <div class="modal-content modal-content-dark">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Новая должность</h5>
-                        <button type="button" class="close text-shadow" data-dismiss="modal" aria-label="Close">
-                            <i class="fas fa-times"></i>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <form method="post" action="{{ route('evoque.admin.roles.add') }}">
-                            @csrf
-                            <div class="form-group">
-                                <label for="title">Название должности</label>
-                                <input type="text" class="form-control" id="title" name="title" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="group">Группа должности</label>
-                                <input type="text" class="form-control" id="group" name="group" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="description">Описание</label>
-                                <input type="text" class="form-control" id="description" name="description">
-                            </div>
-                            <div class="custom-control custom-checkbox mb-3">
-                                <input type="checkbox" class="custom-control-input" id="visible" name="visible" checked>
-                                <label class="custom-control-label" for="visible">Показывать эту роль в таблице</label>
-                            </div>
-                            <button type="submit" class="btn btn-outline-warning">Сохранить</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    @endcan
 
 @endsection

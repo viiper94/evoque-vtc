1bab94479ba0dff07e12c612d358eeb6c00d2fe3
Eugene <viiper94@gmail.com>
2022-09-18 12:41:12 +0300

    added DLC table

---
+++ database/migrations/2022_09_17_174944_create_dlc_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dlc', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('game');
+            $table->text('steam_link')->nullable();
+            $table->integer('sort')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dlc');
+    }
+};

5c21b21181bdaa8e0975c189f26d2cabf6d5e558
Eugene <viiper94@gmail.com>
2022-09-23 11:22:11 +0300

    removed unused template

--- resources/views/evoque/convoys/edit.blade.php
+++ resources/views/evoque/convoys/edit.blade.php
@@ -468,15 +468,4 @@
         });
     </script>
 
-    <script type="text/html" id="route_images_template">
-        <div class="form-group">
-            <div class="custom-file custom-file-dark mb-3">
-                <input type="file" class="custom-file-input" id="route-%i%" name="route[%i%]" accept="image/*">
-                <label class="custom-file-label" for="route-%i%">Выберите изображение</label>
-                <small class="text-primary"><b>Макс. размер файла:</b> 3 Мб, 3000x3000px</small>
-            </div>
-            <img class="w-100" id="route-%i%-preview">
-        </div>
-    </script>
-
 @endsection

5e575907cdedcc8c5065ba3752dc7a22bb167d25
Eugene <viiper94@gmail.com>
2022-09-23 11:12:06 +0300

    added file size check

--- resources/js/modules/common.js
+++ resources/js/modules/common.js
@@ -5,6 +5,13 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 
     $(document).on('change', '.uploader', function(){
+        if(this.files[0].size > 5200000){
+            alert('Превышен лимит размера файла. Максимум 5 МБ!');
+            $(this).val('');
+            $(this).parent().find('label').html('Виберите изображение');
+            $(this).parent().parent().find('img').removeAttr('src');
+            return false;
+        }
         readURL(this, '#' + $(this).attr('id') + '-preview');
     });
 

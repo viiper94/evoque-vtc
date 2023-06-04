6c8c3dd3186a45acfa65242f335f5c73ef699f1a
Eugene <viiper94@gmail.com>
2023-06-04 17:10:34 +0300

    fixed changelog output array keys

--- resources/lang/ru/changelog.php
+++ resources/lang/ru/changelog.php
@@ -4,7 +4,13 @@ $array = [
     'TruckersMP' => 'TruckersMP',
     'TrucksBook' => 'TrucksBook',
     'from' => 'С',
-    'to' => 'До'
+    'to' => 'До',
+    '1' => '1',
+    '2' => '2',
+    '3' => '3',
+    '4' => '4',
+    '5' => '5',
+    '6' => '6',
 ];
 
 $roles = \Illuminate\Support\Facades\Lang::get('roles');
